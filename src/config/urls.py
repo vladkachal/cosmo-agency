1cd2f3fa9e56ef1693b0dca5ab0573c12c02c700
Vlad K. <business@vladk.info>
2026-03-04 13:20:45 +0300

    fix(urls): move static and media patterns from urls_dev to fix DEBUG static serving

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
